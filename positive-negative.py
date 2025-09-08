7a31e5a5fd98d389916bc41f051d83a48940197a
Catalin246 <avorniceseicatalin1234@gmail.com>
2025-09-08 16:50:17 +0200

    Sorting positive/negative numbers.

---
+++ positive-negative.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number < 0:
+    print(f"{number} is a negative number.")
+elif number > 0:
+    print(f"{number} is a positive number.")
+else:
+    print(f"{number} is null value.")
